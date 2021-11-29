730b2aa49a23178efdcdd5fec6036a65032de094
Sheepll <laurakoobas@gmail.com>
2021-11-29 13:18:22 +0200

    Footer fix

---
+++ video.js
@@ -0,0 +1,4 @@
+var obj = {"video": {
+    "value": "<iframe title='YouTube video player' type=\"text/html\" width='640' height='390' src='https://www.youtube.com/embed/SDuyCTox39A' frameborder='0' allowFullScreen></iframe>"
+  }}
+  document.write(obj.video.value);
