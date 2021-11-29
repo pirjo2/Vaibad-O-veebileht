364e3365befa8a5355a6dd5e258f0726907e312e
Sheepll <laurakoobas@gmail.com>
2021-11-29 15:57:49 +0200

    Javascript kommentaarid ja viited

--- kalkulaator.js
+++ kalkulaator.js
@@ -2,14 +2,14 @@
 
 function calcTotal()  
 {
-    
+    //Muutujate väärtuste võtmine HTML failist elemendi ID järgi
     var toode = document.getElementById("toode").value;
     var pikkus = document.getElementById("pikkus").value;
     var laius = document.getElementById("laius").value;
+    //Korrutustehe
     var total = pikkus * laius * toode;
-
+    //HTML failis oleva elemendi muutmine ID järgi, ümardamine kahe komakohani
     document.getElementById("result").innerHTML = "€" + total.toFixed(2);
-    console.log(total)
 
 
 }
--- video.js
+++ video.js
@@ -1,4 +1,8 @@
+
+//Video lehele kinnitamine lingi abil
 var obj = {"video": {
     "value": "<iframe title='YouTube video player' type=\"text/html\" width='640' height='390' src='https://www.youtube.com/embed/SDuyCTox39A' frameborder='0' allowFullScreen></iframe>"
   }}
   document.write(obj.video.value);
+
+  //Lahendus võetud https://stackoverflow.com/questions/22066398/embedding-a-youtube-video-on-page-using-javascript , kasutaja Unknown esimene variant.
